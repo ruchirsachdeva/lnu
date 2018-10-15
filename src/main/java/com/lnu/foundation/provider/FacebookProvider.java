b41501aafedaff0948c6c777ac8483f6209a8a11
Ruchir Sachdeva <ruchir.sachdeva@unibet.com>
2018-10-15 12:38:50 +0200

    bio field is deprecated for favebook versions v2.8 and higher, so removing it

--- src/main/java/com/lnu/foundation/provider/FacebookProvider.java
+++ src/main/java/com/lnu/foundation/provider/FacebookProvider.java
@@ -10,36 +10,37 @@ import org.springframework.ui.Model;
 
 
 @Service
-public class FacebookProvider  {
+public class FacebookProvider {
 
-	private static final String FACEBOOK = "facebook";
-	private static final String REDIRECT_LOGIN = "redirect:/login";
+    private static final String FACEBOOK = "facebook";
+    private static final String REDIRECT_LOGIN = "redirect:/login";
 
-    	@Autowired
-    	BaseProvider baseProvider ;
-    	
+    @Autowired
+    BaseProvider baseProvider;
 
-	public String getFacebookUserData(Model model, UserBean userForm) {
 
-		ConnectionRepository connectionRepository = baseProvider.getConnectionRepository();
-		if (connectionRepository.findPrimaryConnection(Facebook.class) == null) {
-			return REDIRECT_LOGIN;
-		}
-		populateUserDetailsFromFacebook(userForm);
-		model.addAttribute("loggedInUser",userForm);
-		return "user";
-	}
+    public String getFacebookUserData(Model model, UserBean userForm) {
 
-	protected void populateUserDetailsFromFacebook(UserBean userForm) {
-		Facebook facebook = baseProvider.getFacebook();
-		User user = facebook.userOperations().getUserProfile();
-		userForm.setEmail(user.getEmail());
-		userForm.setFirstName(user.getFirstName());
-		userForm.setLastName(user.getLastName());
-		userForm.setImage(user.getCover().getSource());
-		userForm.setProvider(FACEBOOK);
-	}
+        ConnectionRepository connectionRepository = baseProvider.getConnectionRepository();
+        if (connectionRepository.findPrimaryConnection(Facebook.class) == null) {
+            return REDIRECT_LOGIN;
+        }
+        populateUserDetailsFromFacebook(userForm);
+        model.addAttribute("loggedInUser", userForm);
+        return "user";
+    }
 
-	 
+    protected void populateUserDetailsFromFacebook(UserBean userForm) {
+        Facebook facebook = baseProvider.getFacebook();
+        String[] fields = {"id", "email", "first_name", "last_name", "cover"};
+        User user = facebook.fetchObject("me", User.class, fields);
+        //	User user = facebook.userOperations().getUserProfile();
+        userForm.setEmail(user.getEmail());
+        userForm.setFirstName(user.getFirstName());
+        userForm.setLastName(user.getLastName());
+        userForm.setImage(user.getCover().getSource());
+        userForm.setProvider(FACEBOOK);
+    }
+
 
 }
